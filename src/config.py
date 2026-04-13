90ab6e010f43acc870fdeb165418c38008e6a8b2
mateomoliina <moligari55@gmail.com>
2026-04-13 09:20:57 +0200

    mas aeropuertos

--- src/config.py
+++ src/config.py
@@ -1,7 +1,21 @@
 BASE_URL = "https://api.open-meteo.com/v1/forecast"
 
 CITIES = {
-    "Madrid Barajas": {"lat": 40.4722, "lon": -3.5608}
+  "Madrid Barajas": {"lat": 40.4722, "lon": -3.5608},
+  "Barcelona El Prat": {"lat": 41.2974, "lon": 2.0833},
+  "Palma de Mallorca": {"lat": 39.5517, "lon": 2.7388},
+  "Málaga Costa del Sol": {"lat": 36.6749, "lon": -4.4991},
+  "Alicante Elche": {"lat": 38.2822, "lon": -0.5582},
+  "Gran Canaria": {"lat": 27.9319, "lon": -15.3866},
+  "Tenerife Sur": {"lat": 28.0445, "lon": -16.5725},
+  "Valencia": {"lat": 39.4893, "lon": -0.4816},
+  "Sevilla": {"lat": 37.4180, "lon": -5.8931},
+  "Bilbao": {"lat": 43.3011, "lon": -2.9106},
+  "Ibiza": {"lat": 38.8729, "lon": 1.3731},
+  "Lanzarote": {"lat": 28.9455, "lon": -13.6052},
+  "Fuerteventura": {"lat": 28.4527, "lon": -13.8638},
+  "Menorca": {"lat": 39.8626, "lon": 4.2186},
+  "Santiago de Compostela": {"lat": 42.8963, "lon": -8.4151}
 }
 
 WEATHER_PATH = "data/raw/weather_data.json"
